f1fd1d68204f86db981ec2cfdc53ea1738b360fb
Saransivakumar89 <saransivakumar89@gmail.com>
2025-02-22 12:22:29 +0530

    Updated full implementation

--- capsled.c
+++ capsled.c
@@ -1,25 +1,62 @@
-#include<linux/module.h>
-#include<linux/kernel.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/delay.h>
+#include <linux/init.h>
+#include <linux/tty.h>
+#include <linux/console.h>
+#include <linux/vt_kern.h>
+#include <linux/workqueue.h>
 
+#define BLINK_COUNT 10   /* Number of LED blinks */
+#define DELAY 500        /* Delay for LED ON/OFF in milliseconds */
 
-#undef pr_fmt
-#define pr_fmt(fmt) "%s :" fmt,__func__
+static struct work_struct capsled_work;  /* Workqueue task */
 
-    
+/* Function to control LED */
+static void set_leds(int state)
+{
+    struct tty_struct *tty;
+
+    tty = vc_cons[fg_console].d->port.tty;
+    if (tty && tty->ops->ioctl)
+        tty->ops->ioctl(tty, KDSETLED, state);
+}
+
+/* Function to blink the LED */
+static void blink_led(struct work_struct *work)
+{
+    int i;
+    for (i = 0; i < BLINK_COUNT; i++) 
+    {
+        set_leds(0x04);  /* Caps Lock ON */
+        msleep(DELAY);
+        set_leds(0x00);  /* Caps Lock OFF */
+        msleep(DELAY);
+    }
+}
+
+/* Module Init */
 static int __init led_init(void)
 {
-    pr_info("Caps led module loaded\n");
+    pr_info("Caps LED module loaded\n");
+    INIT_WORK(&capsled_work, blink_led);
+    schedule_work(&capsled_work);
     return 0;
 }
 
+/* Module Exit */
 static void __exit led_exit(void)
 {
-    pr_info("caps led module unloaded\n");
+    
+    cancel_work_sync(&capsled_work);
+    pr_info("Caps LED module unloaded\n");
 }
 
 module_init(led_init);
 module_exit(led_exit);
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Saran sivakumar");
-MODULE_DESCRIPTION("Capslock key led blink module");
+MODULE_AUTHOR("Saran Sivakumar");
+MODULE_DESCRIPTION("Simple Caps Lock LED blink module");
+
+
--- capsled.mod.c
+++ capsled.mod.c
@@ -35,12 +35,28 @@ MODULE_INFO(retpoline, "Y");
 
 static const char ____versions[]
 __used __section("__versions") =
-	"\x1c\x00\x00\x00\xca\x39\x82\x5b"
-	"__x86_return_thunk\0\0"
 	"\x14\x00\x00\x00\xbb\x6d\xfb\xbd"
 	"__fentry__\0\0"
+	"\x14\x00\x00\x00\xd3\x85\x33\x2d"
+	"system_wq\0\0\0"
+	"\x18\x00\x00\x00\x36\xf2\xb6\xc5"
+	"queue_work_on\0\0\0"
+	"\x14\x00\x00\x00\xa7\x8e\x6e\x4e"
+	"fg_console\0\0"
+	"\x10\x00\x00\x00\x23\x28\xce\x4e"
+	"vc_cons\0"
+	"\x24\x00\x00\x00\x97\x70\x48\x65"
+	"__x86_indirect_thunk_rax\0\0\0\0"
+	"\x10\x00\x00\x00\xf9\x82\xa4\xf9"
+	"msleep\0\0"
+	"\x28\x00\x00\x00\xb3\x1c\xa2\x87"
+	"__ubsan_handle_out_of_bounds\0\0\0\0"
+	"\x1c\x00\x00\x00\xfe\x2d\xc1\x03"
+	"cancel_work_sync\0\0\0\0"
 	"\x10\x00\x00\x00\x7e\x3a\x2c\x12"
 	"_printk\0"
+	"\x1c\x00\x00\x00\xca\x39\x82\x5b"
+	"__x86_return_thunk\0\0"
 	"\x18\x00\x00\x00\xd7\xd3\x75\x6d"
 	"module_layout\0\0\0"
 	"\x00\x00\x00\x00\x00\x00\x00\x00";
@@ -48,4 +64,4 @@ __used __section("__versions") =
 MODULE_INFO(depends, "");
 
 
-MODULE_INFO(srcversion, "C587ECE11C773CB79AEBF7A");
+MODULE_INFO(srcversion, "A8195A874B8480FCB991890");
